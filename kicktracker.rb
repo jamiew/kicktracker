85c48f4e92cf58f3471f979e1b9988ff7bee5f16
Jamie Wilkinson <jamie@jamiedubs.com>
2011-07-10 15:59:36 -0700

    Make an output.html file that graphs raised vs. total

--- kicktracker.rb
+++ kicktracker.rb
@@ -1,3 +1,4 @@
+##
 #!/usr/bin/env ruby
 
 require 'rubygems'
@@ -5,10 +6,14 @@ require 'mechanize'
 require 'pp'
 
 # URL to your Kickstarter project
-url = "http://www.kickstarter.com/projects/571943958/tempt1-and-eyewriter-art-by-eyes"
+dirname = File.expand_path(File.dirname(__FILE__))
+config = YAML.load(File.open(dirname+'/config.yml').read)
+raise "You must specify a Kickstarter project url in config.yml" if config.nil? || config['url'].empty?
+url = config['url']
 
-filename = File.dirname(__FILE__)+'/results.yml'
-storage = ( File.exists?(filename) ? YAML.load(File.open(filename, 'r')) : [] )
+html_filename = dirname+'/output.html'
+results_filename = dirname+'/results.yml'
+results = ( File.exists?(results_filename) ? YAML.load(File.open(results_filename, 'r')) : [] )
 
 agent = Mechanize.new
 agent.user_agent = "Kicktracker <http://github.com/jamiew/kicktracker>"
@@ -21,16 +26,72 @@ end
 
 totals = {
   :time => DateTime.now.to_s,
-  :backers => counts[0][0].to_i, 
-  :raised => counts[1][0].gsub(',','').gsub('$','').to_i, 
+  :backers => counts[0][0].to_i,
+  :raised => counts[1][0].gsub(',','').gsub('$','').to_i,
   :goal => counts[1][1].match(/of (.*) goal/)[1].gsub('$','').gsub(',','').to_i,
   :days_left => counts[2][0].to_i
 }
+results << totals
 
-puts url
-pp totals
-storage << totals
 
-File.open(filename, 'w+') {|f| f.write(storage.to_yaml) }
-exit 0
 
+# Regenerate output.html
+raised_by_hour = results.group_by{|x| date = DateTime.parse(x[:time]); date.strftime('%m/%y %H') }.map{|k,v| [k, v[-1][:raised]] }.sort_by{|a| a[0] }
+
+total = results[0][:goal]
+html = ''
+
+html += <<-HEREDOC
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>
+      Google Visualization API Sample
+    </title>
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load('visualization', '1', {packages: ['corechart']});
+    </script>
+    <script type="text/javascript">
+      function drawVisualization() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Date');
+        data.addColumn('number', 'Raised');
+        data.addColumn('number', 'Target');
+HEREDOC
+
+last_date = ''
+raised_by_hour.each do |a|
+  arg = a[0].split(' ')[0].gsub(/^0/,'')
+  if last_date == arg
+    label = ''
+  else
+    label = last_date = arg
+  end
+
+  html += "\t\t"
+  html += "data.addRow(['#{label}', #{a[1]}, #{total}]);"
+  html += "\n"
+end
+
+html += <<-HEREDOC
+        new google.visualization.LineChart(document.getElementById('visualization')).
+            draw(data, {curveType: "function", width: 500, height: 400, vAxis: {maxValue: 10}});
+      }
+      google.setOnLoadCallback(drawVisualization);
+    </script>
+  </head>
+  <body style="font-family: Arial;border: 0 none;">
+    <div id="visualization" style="width: 500px; height: 400px;"></div>
+  </body>
+</html>
+HEREDOC
+
+File.open(html_filename, 'w+') {|f| f.write(results.to_yaml) }
+# puts html
+
+
+# Write to results.yml & print
+File.open(results_filename, 'w+') {|f| f.write(results.to_yaml) }
+pp results
