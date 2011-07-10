f7bfa19e5ab2ca51a405e124ed4ca46f50333e55
Jamie Wilkinson <jamie@jamiedubs.com>
2011-07-10 16:44:45 -0700

    Sexify HTML output

--- kicktracker.rb
+++ kicktracker.rb
@@ -13,8 +13,8 @@ url = config['url']
 
 # Graph options
 html_filename = dirname+'/output.html'
-width = config['width'] && config['width'] || 750
-height = config['height'] && config['height'] || 400
+width = config['width'] && config['width'] || 600
+height = config['height'] && config['height'] || 500
 
 # Results
 results_filename = dirname+'/results.yml'
@@ -28,6 +28,9 @@ money = (page/'#moneyraised')
 counts = (money/'h5').map do |stat|
   stat.content.split("\n").select {|n| !n.nil? && !n.empty? }.compact
 end
+
+title = (page/'#name')[0].content
+byline = (page/'#headrow p')[0].content.split('•')[0]
 
 totals = {
   :time => DateTime.now.to_s,
@@ -43,16 +46,20 @@ results << totals
 # Regenerate output.html
 raised_by_hour = results.group_by{|x| date = DateTime.parse(x[:time]); date.strftime('%m/%y %H') }.map{|k,v| [k, v[-1][:raised]] }.sort_by{|a| a[0] }
 
-total = results[0][:goal]
-html = ''
+total = results[-1][:goal]
+raised = results[-1][:raised]
+remaining = total - raised
+backers = results[-1][:backers]
+days_left = results[-1][:days_left]
+updated_at = DateTime.parse(results[-1][:time])
 
-html += <<-HEREDOC
+html = <<-HEREDOC
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title>
-      Google Visualization API Sample
+      Kicktracker - #{title}
     </title>
     <script type="text/javascript" src="http://www.google.com/jsapi"></script>
     <script type="text/javascript">
@@ -90,9 +97,45 @@ html += <<-HEREDOC
       }
       google.setOnLoadCallback(drawVisualization);
     </script>
+    <style type="text/css">
+      #wrapper { margin: 0 auto; width: #{width}px; }
+      #info { margin-left: 70px; }
+      a { color: #000; }
+      h1#title { font-size: 15pt; margin-bottom: 5px; }
+      h2#byline { font-size: 13pt; font-weight: normal; font-style: italic; margin-top: 0; margin-bottom: 20px; }
+      ul.stats { padding-left: 16px; font-size: 11pt; }
+      #updated_at { margin-top: 30px; font-size: 10pt; color: #888; }
+    </style>
   </head>
   <body style="font-family: Arial;border: 0 none;">
-    <div id="visualization" style="width: #{width}px; height: #{height}px;"></div>
+    <div id="wrapper">
+      <div id="visualization" style="width: #{width}px; height: #{height}px;"></div>
+
+      <div id="info">
+        <h1 id="title"><a href="#{url}">#{title}</a></h1>
+        <h2 id="byline">#{byline}</h2>
+
+        <ul class="stats">
+          <li>$#{raised} raised</li>
+          <li>#{(raised.to_f/total.to_f*100.0).round}% complete</li>
+        </ul>
+
+        <ul class="stats">
+          <li>#{backers} backers</li>
+          <li>Avg $#{(raised.to_f/backers.to_f).round}/backer</li>
+        </ul>
+
+        <ul class="stats">
+          <li>#{days_left} days left</li>
+          <li>Goal: $#{(remaining.to_f/days_left.to_f).round} per day</li>
+        </ul>
+
+        <div id="updated_at">
+          Last updated #{updated_at.strftime('%D %T')}
+        </div>
+
+      </div>
+    </div>
   </body>
 </html>
 HEREDOC
@@ -103,4 +146,5 @@ File.open(html_filename, 'w+') {|f| f.write(html) }
 
 # Write to results.yml & print
 File.open(results_filename, 'w+') {|f| f.write(results.to_yaml) }
-pp results
+puts url
+pp totals
